30a342cac5f126b0c464f1572b769d55b3f34f37
Matthew Goff <mtthwgff@gmail.com>
2018-12-15 14:59:39 -0500

    Added right click autopilot

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,8 @@
 
 public class PlayerController : CombatantManager
 {
-    public GameObject ExhaustGlow;
-    public GameObject ExhaustParticles;
+    public GameObject MoveTargetEffect;
+    public GameObject ExhaustEffect;
 
     private readonly float Acceleration = 1000f;
     private readonly float TurnRate = 360f; // Degrees per Second
@@ -11,7 +11,13 @@ public class PlayerController : CombatantManager
 
     private Rigidbody2D RB2D;
     private Vector2 Heading;
+    private Vector2 MoveTarget;
+    private bool HasMoveTarget;
     private int AttackType;
+
+    private bool ThrustInput;
+    private bool BreakInput;
+    private float TurnInput;
 
     private void Awake()
     {
@@ -19,34 +25,48 @@ public class PlayerController : CombatantManager
         Heading = new Vector2(1, 0);
         Team = 0;
         AttackType = 1;
+        HasMoveTarget = false;
     }
 
     private void Update()
     {
-        float turnInput = -Input.GetAxis("Horizontal");
-        Heading = Quaternion.Euler(new Vector3(0, 0, turnInput * Time.deltaTime * TurnRate)) * Heading;
-        transform.eulerAngles = new Vector3(0, 0, Vector2.SignedAngle(Vector2.right, Heading));
-        if (Input.GetKey(KeyCode.W))
+        TurnInput = -Input.GetAxis("Horizontal");
+        ThrustInput = Input.GetKey(KeyCode.W);
+        BreakInput = Input.GetKey(KeyCode.S);
+
+        if (TurnInput != 0f || ThrustInput || BreakInput)
         {
-            RB2D.AddForce(Heading.normalized * Acceleration * Time.deltaTime);
-            ExhaustGlow.SetActive(true);
-            ExhaustParticles.GetComponent<ParticleSystem>().Play();
-        }
-        else
-        {
-            ExhaustGlow.SetActive(false);
-            ExhaustParticles.GetComponent<ParticleSystem>().Stop();
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            RB2D.AddForce(-RB2D.velocity.normalized * Acceleration * Time.deltaTime);
+            DismissMoveTarget();
         }
 
-        if (RB2D.velocity.magnitude > MaximumSpeed)
+        if (Input.GetMouseButton(1))
         {
-            RB2D.velocity = RB2D.velocity.normalized * MaximumSpeed;
+            MoveTarget = MasterCameraController.GetMousePosition();
+            HasMoveTarget = true;
+            MoveTargetEffect.SetActive(true);
+            MoveTargetEffect.transform.position = MoveTarget;
         }
-        
+
+        if (HasMoveTarget)
+        {
+            Vector2 targetBearing = MoveTarget - (Vector2)transform.position;
+            Vector2 velocitySchedule = targetBearing.normalized - RB2D.velocity.normalized;
+            float angle = Vector2.SignedAngle(Heading, velocitySchedule);
+            angle = Mathf.Clamp(angle, -TurnRate * Time.deltaTime, TurnRate * Time.deltaTime);
+            TurnInput = angle / (TurnRate * Time.deltaTime);
+            ThrustInput = true;
+            if (Vector2.Angle(RB2D.velocity, velocitySchedule) < 90f)
+            {
+                ThrustInput = true;
+            }
+            else
+            {
+                //BreakInput = true;
+            }
+        }
+
+        ExhaustEffect.SetActive(ThrustInput);
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             AttackType = 1;
@@ -80,6 +100,34 @@ public class PlayerController : CombatantManager
                 new EMPAttackManager(this, RB2D.position, damage);
             }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (ThrustInput)
+        {
+            RB2D.AddForce(Heading.normalized * Acceleration * Time.deltaTime);
+        }
+        if (BreakInput)
+        {
+            RB2D.AddForce(-RB2D.velocity.normalized * Acceleration * Time.deltaTime);
+        }
+        if (TurnInput != 0)
+        {
+            Heading = Quaternion.Euler(new Vector3(0, 0, TurnInput * Time.deltaTime * TurnRate)) * Heading;
+            transform.eulerAngles = new Vector3(0, 0, Vector2.SignedAngle(Vector2.right, Heading));
+        }
+
+        if (RB2D.velocity.magnitude > MaximumSpeed)
+        {
+            RB2D.velocity = RB2D.velocity.normalized * MaximumSpeed;
+        }
+    }
+
+    private void DismissMoveTarget()
+    {
+        HasMoveTarget = false;
+        MoveTargetEffect.SetActive(false);
     }
 
     public override Vector2 GetPosition()
